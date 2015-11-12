75a31db8a3a961c1b55f7f677b4c2588c18c4df0
核心力量 <kernelforce@gmail.com>
2015-11-12 21:21:01 +0800

    Update interface is available.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

---
+++ src/org/kernelab/dougong/core/dml/Update.java
@@ -0,0 +1,18 @@
+package org.kernelab.dougong.core.dml;
+
+import org.kernelab.dougong.core.Column;
+import org.kernelab.dougong.core.Expression;
+import org.kernelab.dougong.core.View;
+
+public interface Update extends DML, Filterable
+{
+	public Update update(View view);
+
+	// //////////////////////////////////////////////////////
+
+	public Update set(Column column, Expression expr);
+
+	// //////////////////////////////////////////////////////
+
+	public Update where(Condition cond);
+}
