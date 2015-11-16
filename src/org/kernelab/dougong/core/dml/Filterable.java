516c70a2f9b0a02ab0decaa47c50bc0c8737415c
核心力量 <kernelforce@gmail.com>
2015-11-16 21:22:44 +0800

    Sourced interface is available.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

--- src/org/kernelab/dougong/core/dml/Filterable.java
+++ src/org/kernelab/dougong/core/dml/Filterable.java
@@ -2,21 +2,8 @@ package org.kernelab.dougong.core.dml;
 
 import org.kernelab.dougong.core.View;
 
-public interface Filterable
+public interface Filterable extends Sourced
 {
-	/**
-	 * Get the view to be filtered.
-	 * 
-	 * @return
-	 */
-	public View from();
-
-	/**
-	 * Set the view to be filtered.
-	 * 
-	 * @param view
-	 * @return
-	 */
 	public Filterable from(View view);
 
 	/**
