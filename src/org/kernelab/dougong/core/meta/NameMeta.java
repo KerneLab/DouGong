d9e79b6369fe69abb6af8c47efcccefec818b9b5
核心力量 <kernelforce@gmail.com>
2015-12-22 22:28:15 +0800

    NameMeta is available to specify the name of the object.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

---
+++ src/org/kernelab/dougong/core/meta/NameMeta.java
@@ -0,0 +1,15 @@
+package org.kernelab.dougong.core.meta;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface NameMeta
+{
+	public String name() default "";
+}
