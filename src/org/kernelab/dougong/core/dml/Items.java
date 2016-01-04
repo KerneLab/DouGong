5a69e9d270ff35217065cd5def22d00587804e3a
核心力量 <kernelforce@gmail.com>
2016-01-04 20:40:46 +0800

    Alias interface now has been removed from Items.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

--- src/org/kernelab/dougong/core/dml/Items.java
+++ src/org/kernelab/dougong/core/dml/Items.java
@@ -1,12 +1,11 @@
 package org.kernelab.dougong.core.dml;
 
-import org.kernelab.dougong.core.Alias;
 import org.kernelab.dougong.core.Expression;
 import org.kernelab.dougong.core.Scope;
 import org.kernelab.dougong.core.dml.test.EqualityTestable;
 import org.kernelab.dougong.core.dml.test.MembershipTestable;
 
-public interface Items extends Expression, Scope, Alias, EqualityTestable, MembershipTestable
+public interface Items extends Expression, Scope, EqualityTestable, MembershipTestable
 {
 	public Items list(Expression... items);
 }
