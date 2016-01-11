16da11f67bc53986a62595ef69db4e01870a0bee
核心力量 <kernelforce@gmail.com>
2016-01-11 21:34:37 +0800

    Function now be moved to core package.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

---
+++ src/org/kernelab/dougong/core/Function.java
@@ -0,0 +1,11 @@
+package org.kernelab.dougong.core;
+
+import org.kernelab.dougong.core.dml.Item;
+import org.kernelab.dougong.core.dml.Sortable;
+
+public interface Function extends Named, Item, Sortable, Member, Providable
+{
+	public Expression[] args();
+
+	public Function call(Expression... args);
+}
