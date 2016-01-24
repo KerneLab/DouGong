9afe185327374ddf96a0237225583a1832545da0
核心力量 <kernelforce@gmail.com>
2016-01-24 18:20:04 +0800

    OracleSelect now wrapped a limited select object in prepare method.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

--- src/org/kernelab/dougong/orcl/dml/OracleSelect.java
+++ src/org/kernelab/dougong/orcl/dml/OracleSelect.java
@@ -12,40 +12,53 @@ import org.kernelab.dougong.semi.dml.AbstractSelect;
 public class OracleSelect extends AbstractSelect
 {
 	@Override
+	protected OracleSelect prepare()
+	{
+		Expression rows = this.rows();
+
+		if (rows != null)
+		{
+			String rn = "+DouGong-Limit*Orcl/RowNum|";
+			String col = '"' + rn + '"';
+			Item rownum = provider().provideStringItem("ROWNUM");
+
+			Expression skip = this.skip();
+
+			if (skip == null)
+			{
+				skip = provider().provideStringItem("0");
+			}
+
+			Select inner = this.as("t");
+			inner.limit(null, null);
+
+			Select semi = provider().provideSelect() //
+					.from(inner) //
+					.select(inner.all(), //
+							rownum.as(rn)) //
+					.where(rownum.le(skip.plus(rows))) //
+			;
+
+			List<Expression> list = new LinkedList<Expression>();
+
+			list.addAll(semi.items().values());
+			list.remove(list.size() - 1);
+
+			return (OracleSelect) provider().provideSelect() //
+					.from(semi) //
+					.select(list.toArray(new Expression[list.size()])) //
+					.where(provider().provideStringItem(col).gt(skip));
+		}
+		else
+		{
+			return this;
+		}
+	}
+
+	@Override
 	public OracleSelect provider(Provider provider)
 	{
 		super.provider(provider);
 		return this;
-	}
-
-	public Select limit(Expression skip, Expression rows)
-	{
-		String rn = "+DouGong-Limit*Orcl/RowNum|";
-		String col = '"' + rn + '"';
-		Item rownum = provider().provideStringItem("ROWNUM");
-
-		if (skip == null)
-		{
-			skip = provider().provideStringItem("0");
-		}
-
-		Select inner = this.as("t");
-
-		Select semi = provider().provideSelect() //
-				.from(inner) //
-				.select(inner.all(), //
-						rownum.as(rn)) //
-				.where(rownum.le(skip.plus(rows))) //
-		;
-
-		List<Expression> list = new LinkedList<Expression>();
-
-		list.addAll(semi.items().values());
-		list.remove(list.size() - 1);
-
-		return provider().provideSelect() //
-				.from(semi) //
-				.select(list.toArray(new Expression[list.size()])) //
-				.where(provider().provideStringItem(col).gt(skip));
 	}
 }
