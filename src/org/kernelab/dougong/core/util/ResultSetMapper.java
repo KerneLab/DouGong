6c9839b8c240f6f74dbecb32c73cbfe0c1e84138
核心力量 <kernelforce@gmail.com>
2016-04-19 23:57:51 +0800

    ResultSetMapper is available which could be used in ResultSetIterator.
    Signed-off-by: 核心力量 <kernelforce@gmail.com>

---
+++ src/org/kernelab/dougong/core/util/ResultSetMapper.java
@@ -0,0 +1,48 @@
+package org.kernelab.dougong.core.util;
+
+import java.lang.reflect.Field;
+import java.sql.ResultSet;
+import java.util.Map;
+
+import org.kernelab.basis.Mapper;
+
+public class ResultSetMapper<T> implements Mapper<ResultSet, T>
+{
+	private Class<T>			cls;
+
+	private Map<Field, String>	map;
+
+	public Class<T> getCls()
+	{
+		return cls;
+	}
+
+	public Map<Field, String> getMap()
+	{
+		return map;
+	}
+
+	public T map(ResultSet rs)
+	{
+		try
+		{
+			return Utils.mapResultSetToObject(rs, cls.newInstance(), map);
+		}
+		catch (Exception e)
+		{
+			return null;
+		}
+	}
+
+	public ResultSetMapper<T> setCls(Class<T> cls)
+	{
+		this.cls = cls;
+		return this;
+	}
+
+	public ResultSetMapper<T> setMap(Map<Field, String> map)
+	{
+		this.map = map;
+		return this;
+	}
+}
